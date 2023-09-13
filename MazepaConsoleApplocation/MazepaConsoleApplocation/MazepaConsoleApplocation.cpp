0d5f559119d6c557c1e272c47fb4ef55b78d4cc8
SCP-1499 <91142558+SCP-1499@users.noreply.github.com>
2023-09-13 19:17:49 +0300

    Version 0.1
    A project has been created - a console application. Added the output of the string "Hello world" to the console, as well as a request for character input.

---
+++ MazepaConsoleApplocation/MazepaConsoleApplocation/MazepaConsoleApplocation.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{   
+    char a;
+    cout << "Hello World!\n";
+    cout << "Enter a character\n";
+    cin >> a;
+    cout << "The entered character: \n";
+    cout << a;
+}
